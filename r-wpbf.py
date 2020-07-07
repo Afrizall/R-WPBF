5ff58a0d48629f835f042f7c39a8a87687b3fdc3
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-07-08 06:53:50 +0700

    Update r-wpbf.py

--- r-wpbf.py
+++ r-wpbf.py
@@ -23,7 +23,7 @@ class rusher_wpbf:
     def useragent(self):
 
         arr = ["Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.464.0 Safari/534.3", "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16", "Mozilla/5.0 (X11; U; FreeBSD i386; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.207.0 Safari/532.0", "Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.1 (KHTML, like Gecko) Chrome/6.0.427.0 Safari/534.1"]
-        return arr[random.randint(0,len(arr)-1)]
+        return arr[random.randint(0, len(arr) - 1)]
 
     def check_xmlrpc(self, target):
 
